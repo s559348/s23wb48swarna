0dd6a4d03b39843ddc4858c39b88e606236d6078
s559348 <ashok.swarna1995@gmail.com>
2023-04-05 18:56:23 -0500

    update

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var swarnaRouter = require('./routes/shirt');
+var selectRouter = require('./routes/selector');
 
 var app = express();
 
@@ -23,6 +24,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/shirt', swarnaRouter);
+app.use('/selector', selectRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
